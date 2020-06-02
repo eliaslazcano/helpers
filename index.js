43a404ca65547047db7d4c81fafc3d3ccb319f3d
Elias Lazcano Castro Neto <elias304162@gmail.com>
2020-06-01 20:01:06 -0400

    Pasta src para lib
    Pasta renomeada

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import DateHelper from "./src/DateHelper";
-import BrazilHelper from "./src/BrazilHelper";
-import JwtHelper from "./src/JwtHelper";
-import StringHelper from "./src/StringHelper";
-import ImageHelper from "./src/ImageHelper";
+import DateHelper from "./lib/DateHelper";
+import BrazilHelper from "./lib/BrazilHelper";
+import JwtHelper from "./lib/JwtHelper";
+import StringHelper from "./lib/StringHelper";
+import ImageHelper from "./lib/ImageHelper";
 
 export {DateHelper, BrazilHelper, JwtHelper, StringHelper, ImageHelper};
